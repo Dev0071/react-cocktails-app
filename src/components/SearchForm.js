7b176a9ab84c2303080e373c267bf99eb4ad8213
Dev0071 <lecturergacheru71@gmail.com>
2023-03-17 14:16:59 +0300

    implemented the search functionality on the serachForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
-  const { setsearchTerm } = useGlobalContext();
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef('');
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='section search'>
+      <form className='search-form'>
+        <div className='form-control'>
+          <label htmlFor='name'>Search your favourite cocktail</label>
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
