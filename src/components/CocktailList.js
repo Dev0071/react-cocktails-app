c2aeb32302f4cc315340356885717b068e3ab000
Dev0071 <lecturergacheru71@gmail.com>
2023-06-16 23:46:58 +0300

    refactored code

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,37 +4,35 @@ import Loading from './Loading';
 import { useGlobalContext } from '../context';
 
 const CocktailList = () => {
-  const { loading, cocktails } = useGlobalContext();
-  // console.log(loading);
-  // console.log(cocktails);
+	const { loading, cocktails } = useGlobalContext();
 
-  if (loading) {
-    return <Loading />;
-  }
+	if (loading) {
+		return <Loading />;
+	}
 
-  if (cocktails.length < 1) {
-    return (
-      <h2 className='section-title'>
-        No cocktails matched your search criteria
-      </h2>
-    );
-  }
+	if (cocktails.length < 1) {
+		return (
+			<h2 className='section-title'>
+				No cocktails matched your search criteria
+			</h2>
+		);
+	}
 
-  return (
-    <section className='section'>
-      <h2 className='section-title'>cocktails</h2>
-      <div className='cocktails-center'>
-        {cocktails.map((drink) => {
-          return (
-            <Cocktail
-              key={drink.id}
-              {...drink}
-            />
-          );
-        })}
-      </div>
-    </section>
-  );
+	return (
+		<section className='section'>
+			<h2 className='section-title'>cocktails</h2>
+			<div className='cocktails-center'>
+				{cocktails.map((drink) => {
+					return (
+						<Cocktail
+							key={drink.id}
+							{...drink}
+						/>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default CocktailList;
